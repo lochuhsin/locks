4982a5482f9340eb577af9b73319fbc232b4758a
lochuhsin <lochuhsin@gmail.com>
2024-07-03 21:13:57 +0800

    feat(chore): add comments

--- lock.go
+++ lock.go
@@ -5,6 +5,31 @@ import (
 	"sync/atomic"
 )
 
+/**
+ * Simple but sometimes not efficient, since other goroutine should be waiting for lock
+ */
+func mutexlock(target int64) int64 {
+	var mu sync.Mutex
+	var count int64 = 0
+	group := &sync.WaitGroup{}
+	group.Add(int(target))
+	for i := 0; i < int(target); i++ {
+		go func(group *sync.WaitGroup) {
+			mu.Lock()
+			count++
+			mu.Unlock()
+			group.Done()
+		}(group)
+	}
+	group.Wait()
+	return count
+}
+
+/**
+ * Since cas doesn't acquire lock in general, it is more performant to use.
+ * However, the threads that are not able to add value will stuck in the infinite loop
+ * which waste a lot of CPU clocks. (Spinning)
+ */
 func caslock(target int64) int64 {
 	var count int64 = 0
 	group := &sync.WaitGroup{}
@@ -18,23 +43,6 @@ func caslock(target int64) int64 {
 					break
 				}
 			}
-			group.Done()
-		}(group)
-	}
-	group.Wait()
-	return count
-}
-
-func mutexlock(target int64) int64 {
-	var mu sync.Mutex
-	var count int64 = 0
-	group := &sync.WaitGroup{}
-	group.Add(int(target))
-	for i := 0; i < int(target); i++ {
-		go func(group *sync.WaitGroup) {
-			mu.Lock()
-			count++
-			mu.Unlock()
 			group.Done()
 		}(group)
 	}
